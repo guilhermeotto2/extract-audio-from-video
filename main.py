9682f6d37ea6cafb7042302d895837b81d0a492b
Guilherme Otto <131212950+guilhermeotto2@users.noreply.github.com>
2023-07-26 08:25:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import glob
 lista_mp4 = glob.glob('*.mp4')
 
 for video in lista_mp4:
-    print('Lendo arquivo mp4')
+    print('Reading mp4 archive')
     print('-'*10)
     mp4 = VideoFileClip(os.path.join(video))
 
-    novo_nome = input(f"Digite o novo nome para o arquivo MP3 (sem a extensão .mp3) do vídeo {video}: ")
+    novo_nome = input(f"Enter the new name for the MP3 file (without the .mp3 extension) for the {video}: ")
 
     nome_mp3 = novo_nome + '.mp3'
-    
-    print('Convertendo para mp3')
-    http://mp4.audio.write_audiofile(os.path.join(nome_mp3))
+
+    print('converting for mp3')
+    mp4.audio.write_audiofile(os.path.join(nome_mp3))
     print('-'*20)
-    print(f'Converteu mp4 {video} para mp3 {nome_mp3}')
+    print(f'converted mp4 {video} para mp3 {nome_mp3}')
